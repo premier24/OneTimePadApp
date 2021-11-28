8e43b9587863dcc6466287a647cf6f79db5d01d3
Prem <Prem@DESKTOP-N2TTIIG>
2021-11-28 21:51:07 +0530

    Add project files.

---
+++ OneTimePadApp.py
@@ -0,0 +1,25 @@
+
+# This is my new python project. Let's see if I've still got it
+# Idea is to complement the cryptography course and write some of the algos to test my knowledge or wahtever
+# This is for the OneTimePad Algorithm here on called as OTP
+
+
+"""
+1. Create message namespace
+2. Create key namespace
+3. Encrypt/ Decrypt.
+"""
+i=1
+while i=1:
+    OneTimePadApp()
+
+    
+
+def OneTimePadApp():
+
+    print( "Please select the following options")
+    print("1. To Encrypt Text")
+    print("2. To decrypt text")
+    # Getting the userinput and checking if it is a valid input
+    SelectedOption=input()
+    if (int[SelectedOption
